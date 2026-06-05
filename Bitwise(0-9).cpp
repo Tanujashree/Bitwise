3122eb8df001524c62d360f69b02743cf3aff10d
Tanujashree <25cb056@rmkec.ac.in>
2026-06-05 21:07:51 +0530

    Refactor digit tracking to use array instead of flag

--- Bitwise(0-9).cpp
+++ Bitwise(0-9).cpp
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <ctype.h>
+
 int main() {
     char str[1000001];
     scanf("%s", str);
 
-    int flag = 0;
+    int digit[10] = {0};
 
     for (int i = 0; str[i]; i++) {
-        if (isdigit(str[i])){
-            flag = flag | (1 << (str[i] - '0'));
-        }
-        else{
-            continue;
+        if (isdigit(str[i])) {
+            digit[str[i] - '0'] = 1;
         }
     }
-    printf((flag == (1<<10)-1) ? "Yes\n" : "No\n");
+
+    int allPresent = 1;
+
+    for (int i = 0; i < 10; i++) {
+        if (digit[i] == 0) {
+            allPresent = 0;
+            break;
+        }
+    }
+
+    if (allPresent)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
     return 0;
 }
